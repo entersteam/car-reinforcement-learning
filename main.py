dc2d6a186c439e541fc11861b8ace53ee40e3073
Yayayaya <105579652+entersteam@users.noreply.github.com>
2024-05-24 16:00:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def rotation_matrix(rad):
 def normalize_vector(v1):
     return v1/ np.linalg.norm(v1)
 
+senselines_rotation_matrixs = np.array([rotation_matrix(-np.pi/3),rotation_matrix(-np.pi/6),rotation_matrix(0),rotation_matrix(np.pi/6),rotation_matrix(np.pi/3)])
+
+right_angnle_rotation_matrix = np.array([[0, -1], [1,0]])
+
 clockwise_rotation_matrix = rotation_matrix(rotation_rad)
 counter_clockwise_rotation_matrix = rotation_matrix(-rotation_rad)
 
+
+def half_angle_vector(v1,v2): # 왼쪽방향 벡터
+    return normalize_vector(right_angnle_rotation_matrix @ (normalize_vector(v1) + normalize_vector(v2)))
+    
 class car:
     def __init__(self, position, direction = np.zeros((2), dtype = float), *args, **kwargs) -> None:
         self.position = position
@@ -35,6 +43,8 @@ class car:
             self.weights_accel = np.random.normal(0, 1, (3,5))
             self.biases_accel = np.random.normal(0, 1, 3)
         
+        self.sense_lines = self.create_senselines()
+        
         self.distances = np.zeros((5), dtype = float)
             
     def update(self):
@@ -46,6 +56,12 @@ class car:
         
         self.position += self.velocity
         self.distance += np.linalg.norm(self.velocity)
+        
+        self.sense_lines = self.create_senselines()
+        
+    def create_senselines(self):
+        return np.array([5 * (matrix @ self.direction) + self.position for matrix in senselines_rotation_matrixs])
+        
             
     def acceleration(self):
         self.velocity += self.direction * ACC
@@ -54,28 +70,34 @@ class car:
         self.velocity -= self.direction * ACC
         
     def turn_right(self):
-        self.direction = clockwise_rotation_matrix @ self.direction
+        self.direction = normalize_vector(clockwise_rotation_matrix @ self.direction)
         
     def turn_left(self):
-        self. direction = counter_clockwise_rotation_matrix @ self.direction
+        self. direction = normalize_vector(counter_clockwise_rotation_matrix @ self.direction)
         
     def do_nothing():
         pass
         
-    def normalize_direction(self):
-        self.direction = normalize_vector(self.direction)
-        
 class envirnoment:
     def __init__(self, points : list) -> None:
-        self.roads = np.array((points, points[1:]+points[:1]))
-        self.roads = np.rot90(self.roads, 3)
+        
+        self.roads = np.concatenate((np.array(points), np.array(points[1:]+points[:1])), axis = 1).reshape(-1,2,3)
         print(self.roads)
         
-        self.init_position = self.roads[0,0]
-        self.init_direction = normalize_vector(self.roads[0,1] - self.roads[0,0])
+        self.init_position = self.roads[0,0,:2]
+        self.init_direction = normalize_vector(self.roads[0,1,:2] - self.roads[0,0,:2])
+        
+        self.walls = np.array([])
+        
         self.cars = []
         for i in range(20):
             self.cars.append(car(self.init_position, self.init_direction))
+            
+    def create_walls(self):
+        for i in range(len(self.roads)):
+            r1 = self.roads[i-1]
+            r2 = self.roads[i]
+            half_angle = half_angle_vector(r1[1,:2] - r1[0,:2], r2[1,:2] - r2[0,:2])
             
 
 if __name__ == "__main__":
